d86c4a8973c7e76f28ca97f6b633832b377a9896
LavrinDmitrovich <dmitrylavrinovich@gmail.com>
2023-05-20 17:16:57 +0300

    Initial commit

---
+++ Snake game/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Snake_game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.snapButton = new System.Windows.Forms.Button();
+            this.picCanvas = new System.Windows.Forms.PictureBox();
+            this.txtScore = new System.Windows.Forms.Label();
+            this.txtHighScore = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.startButton.Location = new System.Drawing.Point(639, 12);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 75);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.StartGame);
+            // 
+            // snapButton
+            // 
+            this.snapButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.snapButton.Location = new System.Drawing.Point(639, 111);
+            this.snapButton.Name = "snapButton";
+            this.snapButton.Size = new System.Drawing.Size(75, 75);
+            this.snapButton.TabIndex = 1;
+            this.snapButton.Text = "SNAP";
+            this.snapButton.UseVisualStyleBackColor = false;
+            this.snapButton.Click += new System.EventHandler(this.TakeSnapShot);
+            // 
+            // picCanvas
+            // 
+            this.picCanvas.BackColor = System.Drawing.Color.White;
+            this.picCanvas.Location = new System.Drawing.Point(24, 21);
+            this.picCanvas.Name = "picCanvas";
+            this.picCanvas.Size = new System.Drawing.Size(580, 680);
+            this.picCanvas.TabIndex = 2;
+            this.picCanvas.TabStop = false;
+            this.picCanvas.Paint += new System.Windows.Forms.PaintEventHandler(this.UpdatePictureBoxGraphics);
+            // 
+            // txtScore
+            // 
+            this.txtScore.AutoSize = true;
+            this.txtScore.Location = new System.Drawing.Point(626, 329);
+            this.txtScore.Name = "txtScore";
+            this.txtScore.Size = new System.Drawing.Size(56, 16);
+            this.txtScore.TabIndex = 3;
+            this.txtScore.Text = "Score: 0";
+            // 
+            // txtHighScore
+            // 
+            this.txtHighScore.AutoSize = true;
+            this.txtHighScore.Location = new System.Drawing.Point(626, 268);
+            this.txtHighScore.Name = "txtHighScore";
+            this.txtHighScore.Size = new System.Drawing.Size(87, 16);
+            this.txtHighScore.TabIndex = 4;
+            this.txtHighScore.Text = "High Score: 0";
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Interval = 40;
+            this.gameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(746, 717);
+            this.Controls.Add(this.txtHighScore);
+            this.Controls.Add(this.txtScore);
+            this.Controls.Add(this.picCanvas);
+            this.Controls.Add(this.snapButton);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Snake";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.KeyIsDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.KeyIsUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picCanvas)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button snapButton;
+        private System.Windows.Forms.PictureBox picCanvas;
+        private System.Windows.Forms.Label txtScore;
+        private System.Windows.Forms.Label txtHighScore;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Snake game/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Snake_game
+{
+    public partial class Form1 : Form
+    {
+        //list related to snake
+        private List<Circle> Snake = new List<Circle> ();
+
+        private Circle food = new Circle ();
+        
+        int maxWidth;
+        int maxHeight;
+        
+        int score;
+        int highScore;
+
+        Random random = new Random ();
+
+        bool goLeft, goRight, goUp, goDown;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void KeyIsDown(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void KeyIsUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void StartGame(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TakeSnapShot(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UpdatePictureBoxGraphics(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        //area of the custom functions (below)
+        private void RestartGame() 
+        { 
+        
+        }
+
+        private void EatFood()
+        { 
+        
+        }
+
+        private void GameOver()
+        {
+
+        }
+
+    }
+}
